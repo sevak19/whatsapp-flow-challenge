6f5e8189cd2e4fe661b71cf33e89b81c9d7285e4
sevak19 <tu1227csn@gmail.com>
2026-06-17 23:19:44 -0300

    Inicializa estrutura do projeto

---
+++ backend/src/main/java/whatsapp_flow_challenge/WhatsappFlowChallengeApplication.java
@@ -0,0 +1,13 @@
+package whatsapp_flow_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappFlowChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappFlowChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/whatsapp_flow_challenge/WhatsappFlowChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package whatsapp_flow_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappFlowChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
